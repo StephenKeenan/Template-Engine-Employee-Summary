18f9d69bc8ae61db8e55cd185328b42928fe540d
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-20 18:50:37 -0500

    Intern.js

--- develop/lib/Intern.js
+++ develop/lib/Intern.js
@@ -1,1 +1,21 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// Defines and exports the Intern class, inheriting from Employee.
+
+const Employee = require("./lib/Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.school = school;
+
+        this.getRole = function() {
+            this.role = "Intern";
+            return this.role;
+        }
+    }
+
+    getSchool() {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
