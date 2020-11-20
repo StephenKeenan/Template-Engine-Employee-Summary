5dd22db8d0554a49bbbe065c0ea014c26e303d75
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-20 18:37:00 -0500

    Employee.js

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,1 +1,20 @@
-// TODO: Write code to define and export the Employee class
+// Defines and exports the Employee class
+class Employee {
+    constructor (name, id, email, phone) {
+        this.name = name; 
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+}
