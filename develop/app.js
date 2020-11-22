da7b6d2789711295ec748483e56cb479add0ae70
Stephen Keenan <stephenkeenan02@gmail.com>
2020-11-21 19:41:41 -0500

    app.js update

--- develop/app.js
+++ develop/app.js
@@ -10,6 +10,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+async function init() {
+    teamInput = new tInput.TeamInput ();
+    await teamInput.generateTeam()
+}
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
